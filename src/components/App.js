fce888b4444e3d627072f8069f7d1b567bfa74ce
kiransahu-dev <61940769+kiransahu-dev@users.noreply.github.com>
2023-03-02 19:33:34 +0530

    updated app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,29 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-const App = () => {
+const App = ({slides}) => {
+  let [slideNum, setSlideNum] = useState(0);
+
+  function prevClicked(){
+    setSlideNum(slideNum-1);
+  }
+  function nextClicked(){
+    setSlideNum(slideNum+1);
+  }
+  function restartClicked(){
+    setSlideNum(0);
+  }
   return (
-    <></>
+    <>
+      <div id="slide">
+        <h1 data-testid="title">{slides[slideNum].title}</h1>
+        <p data-testid="text">{slides[slideNum].text}</p>
+      </div>
+
+      <button disabled = {slideNum == 0} data-testid="button-prev" onClick={prevClicked}>Prev</button>
+      <button disabled = {slideNum == 4} data-testid="button-next" onClick={nextClicked}>Next</button>
+      <button disabled = {slideNum == 0} data-testid="button-restart" onClick={restartClicked}>Restart</button> 
+    </>
   )
 }
 
